dbc2495d4c807fee8c94aa064f6b781f844892dd
Andral-6821 <aluno25069@ipt.pt>
2026-05-12 17:39:41 +0100

    MyUser Model  fixed + Comment Model

---
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/Comment.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VirtualScrap_25069_24169.Data.Model
+{
+    public class Comment
+    {
+        /// <summary>
+        /// Chave primária para cada comentário, será chave estrangeira no perfil do utilizador
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        ///<summary>
+        /// Conteudo do comentário/Avaliação
+        /// </summary>
+        [Display(Name ="Descrição")]
+        [Required(ErrorMessage ="A {0} é de escrita obrigatória")]
+        [StringLength(500)]
+        public string Description { get; set; } = null!;
+
+        ///<summary>
+        ///Data de quando foi feito o comentário
+        /// </summary>
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CommentDate { get; set; } = DateTime.Now;
+
+        ///<summary>
+        ///Autor do Comentário/Avaliação deixada num perfil
+        /// </summary>
+        [ForeignKey(nameof(Autor))]
+        public int AutorFK { get; set; }
+        [ValidateNever]
+
+        ///<summary>
+        ///Objeto do tipo MyUser para o autor
+        ///</summary>
+        public MyUser Autor { get; set; } = null!;
+
+        ///<summary>
+        ///Autor do Comentário/Avaliação deixada num perfil
+        /// </summary>
+        [ForeignKey(nameof(Recipient))]
+        public int RecipientFK { get; set; }
+        [ValidateNever]
+
+        ///<summary>
+        ///Objeto do tipo MyUser para o destinatário
+        ///</summary>
+        public MyUser Recipient { get; set; } = null!;
+    }
+}
--- VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/MyUser.cs
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/MyUser.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VirtualScrap_25069_24169.Data.Model
 {
     public class MyUser
     {
         [Key] //Primary Key
-
+        public int Id { get; set; }
         ///<summary>
         ///Nome do Utilizador.
         ///</summary>
@@ -25,10 +26,36 @@ namespace VirtualScrap_25069_24169.Data.Model
 
 
         ///<summary>
+        ///Lista de comentários feita ao utilizador
+        /// </summary>
+        [InverseProperty(nameof(Comment.Autor))] 
+        public ICollection<Comment> Sent_Comments { get; set; } = [];
+
+
+        ///<summary>
+        ///Lista de comentários feita Pelo utilizador
+        /// </summary>
+        [InverseProperty(nameof(Comment.Recipient))]
+        public ICollection<Comment> Received_Comments { get; set; } = [];
+
+
+
+        ///<summary>
         ///Chave Forasteira para ligar com a tabela de autenticação
         /// </summary>
 
         public string IdUser { get; set; } = null!;
-       
+
+
+        ///<summary>
+        ///Lista de anuncios feitos pelo utilizador
+        /// </summary>
+        public ICollection<Post> PostsList { get; set; } = [];
+        
+
+        ///<summary>
+        ///Lista de likes que este utilizador realizou
+        ///</summary>
+        public ICollection<Like> LikesList { get; set; } = [];
     }
 }
