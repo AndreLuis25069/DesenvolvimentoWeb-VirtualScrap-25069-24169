2f6a7fcb04be8ec27ee7b8bbf9b41f446b757f96
Andral-6821 <aluno25069@ipt.pt>
2026-05-12 16:49:33 +0100

    Classe MyUser

---
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/MyUser.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualScrap_25069_24169.Data.Model
+{
+    public class MyUser
+    {
+        [Key] //Primary Key
+
+        ///<summary>
+        ///Nome do Utilizador.
+        ///</summary>
+
+        [Display(Name="Nome completo do Utilizador")]
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório!")]
+        [StringLength(50)]
+        public string Name { get; set; } = "";
+
+        ///<summary>
+        ///Numero de telefone do Utilizador
+        /// </summary>
+        [Display(Name="Numero de Telefone do Utilizador")]
+        [StringLength(19)]
+        [RegularExpression(@"\+?[0-9]{9,18}",ErrorMessage ="O {0} é de preenchimento obrigatório")]
+        public string CellPhone { get; set; }
+
+
+        ///<summary>
+        ///Chave Forasteira para ligar com a tabela de autenticação
+        /// </summary>
+
+        public string IdUser { get; set; } = null!;
+       
+    }
+}
