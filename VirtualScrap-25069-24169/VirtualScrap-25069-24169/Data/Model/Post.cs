647dab3c4c1f52af965b286c78899ebbba64ce29
Aluno24169 <aluno24169@ipt.pt>
2026-05-13 17:39:19 +0100

    Criação dos models Category e Post (Finalização dos atributos de cada um)

--- VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/Post.cs
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/Post.cs
@@ -1,6 +1,78 @@
-﻿namespace VirtualScrap_25069_24169.Data.Model
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VirtualScrap_25069_24169.Data.Model
 {
     public class Post
     {
+        [Key] //Primary Key
+        public int Id { get; set; }
+
+        ///<summary>
+        ///Título do post (ou nome do produto a ser vendido)
+        ///</summary>
+
+        [Display(Name = "Título")]
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
+        [StringLength(50)]
+        public string Title { get; set; } = "";
+
+
+        ///<summary>
+        ///Descrição do post
+        ///</summary>
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório!")]
+        [StringLength(150)]
+        public string Description { get; set; } = "";
+
+        ///<summary>
+        ///Lista de likes referentes ao post
+        /// </summary>
+
+        public ICollection<Like> LikesList { get; set; } = [];
+
+        ///<summary>
+        ///Contacto do vendedor
+        /// </summary>
+
+        [StringLength(19)]
+        [RegularExpression(@"\+?[0-9]{9,18}", ErrorMessage = "Ocorreu um erro!")]
+        public string CellPhone { get; set; } = "";
+
+        ///<summary>
+        ///Data em que o post foi publicado
+        /// </summary>
+
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime PostDate { get; set; } = DateTime.Now;
+
+        ///<summary>
+        ///Foto do post
+        /// </summary>
+
+        [Display(Name = "Foto")]
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório!")]
+        [StringLength(50)]
+        public string Photo { get; set; } = "";
+
+        ///<summary>
+        ///Categoria do post
+        /// </summary>
+
+        
+        [ValidateNever]
+        public Category PostCategory { get; set; } = null!;
+
+        /// <summary>
+        /// Chave estrangeira para referenciar a categoria
+        /// </summary>
+
+        [Display(Name = "Categoria")]
+        [ForeignKey(nameof(PostCategory))]
+        public int CategoryFK { get; set; }
     }
 }
