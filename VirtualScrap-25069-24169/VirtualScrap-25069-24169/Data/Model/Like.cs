bec0f476572107eecb2078c4fb96b0f4bd76b586
Andral-6821 <aluno25069@ipt.pt>
2026-05-13 17:37:11 +0100

    Like Model

--- VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/Like.cs
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/Like.cs
@@ -1,6 +1,45 @@
-﻿namespace VirtualScrap_25069_24169.Data.Model
-{
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VirtualScrap_25069_24169.Data.Model
+{   
+
+    /// <summary>
+    /// Chave Primaria Composta, para cada like, pois um like é uma ponte entre um utilizador e um anuncio 
+    /// </summary>
+    [PrimaryKey(nameof(LikeAutorFK), nameof(PostFK))]
     public class Like
-    {
+    {   
+       
+        ///<summary>
+        ///Autor do Like deixado num anuncio
+        /// </summary>
+        [ForeignKey(nameof(LikeAutor))]
+        public int LikeAutorFK { get; set; }
+
+
+
+        ///<summary>
+        ///Objeto do tipo MyUser para o autor do like
+        ///</summary>
+         [ValidateNever]
+        public MyUser LikeAutor { get; set; } = null!;
+
+
+        /// <summary>
+        /// Referência ao post a que pertence o like
+        /// </summary>
+        [ForeignKey(nameof(LikedPost))]
+        public int PostFK { get; set; }
+
+
+        ///<summary>
+        ///Objeto do tipo Post/Anuncio
+        ///</summary>
+        [ValidateNever]
+        public Post LikedPost { get; set; } = null!;
+
     }
 }
