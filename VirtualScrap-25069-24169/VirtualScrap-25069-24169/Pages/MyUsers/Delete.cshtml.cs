b27fddf4c14a8a6d172f4e3a67bd1d47b413149c
Andral-6821 <aluno25069@ipt.pt>
2026-06-05 14:27:22 +0100

    Configurações da BD para proceder os deletes de Users

--- VirtualScrap-25069-24169/VirtualScrap-25069-24169/Pages/MyUsers/Delete.cshtml.cs
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Pages/MyUsers/Delete.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
 using VirtualScrap_25069_24169.Data;
 using VirtualScrap_25069_24169.Data.Model;
 
@@ -32,14 +33,21 @@ namespace VirtualScrap_25069_24169.Pages.MyUsers
             //Carrega o Utilizador da base de dados 
             MyUser = await _context.MyUsers
                 //Incluir a lista de posts que esse utilizador tem
-               .Include(u => u.PostsList)
-               .FirstOrDefaultAsync(m => m.Id == id);
+                .Include(p => p.PostsList)
+                .FirstOrDefaultAsync(m => m.Id == id);
+                   
+               
              
             //Se for nulo retorna erro
             if(MyUser == null)
             {
                 return NotFound();
             }
+
+            MyUser.PostsList = await _context.Posts
+                .Where(p => p.OwnerFK == MyUser.Id)
+                .ToListAsync();
+
 
             return Page();
            
@@ -56,11 +64,19 @@ namespace VirtualScrap_25069_24169.Pages.MyUsers
 
             //Vai á base de dados buscar um utilizador com o mesmo id
             var myuser = await _context.MyUsers.FindAsync(id);
+                
             //Se o utilizador não for nulo vai proceder para a elimação do mesmo
             if (myuser != null)
             {
-                 
+                var likesDone = await _context.Likes.Where(l => l.LikeAutorFK == id).ToListAsync();
+                _context.Likes.RemoveRange(likesDone);
+
+                var sentReviews = await _context.Comments.Where(r => r.AutorFK == id).ToListAsync();
+                foreach(var r in sentReviews) { r.AutorFK = null;}
                 MyUser = myuser;
+
+                
+
                 _context.MyUsers.Remove(MyUser);
                 await _context.SaveChangesAsync();
             }
