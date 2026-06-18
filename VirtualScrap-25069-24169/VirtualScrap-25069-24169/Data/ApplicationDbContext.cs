dd2e48636d34e0d133b7a2bfe7c13abce5a273a0
AndreLuis25069 <aluno25069@ipt.pt>
2026-06-18 17:39:38 +0100

    Alterações Simples na navbar, para facilitar o acesso em desenvolvimento.

--- VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/ApplicationDbContext.cs
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace VirtualScrap_25069_24169.Data
         public DbSet<Like> Likes { get; set; }
         public DbSet<PostComment> PostComments { get; set; }
 
+        
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
