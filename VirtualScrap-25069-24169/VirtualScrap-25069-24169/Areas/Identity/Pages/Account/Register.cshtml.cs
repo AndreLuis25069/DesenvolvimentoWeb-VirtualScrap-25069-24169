82569714215c3bc7f628d3f2b832dfa7303ac919
AndreLuis25069 <aluno25069@ipt.pt>
2026-06-23 00:57:38 +0100

    Configuração dos utilizadores para o DBInitializer, cria 2 administradores e 3 users comuns sempre que a BD está vazia nas tabelas ligadas.

--- VirtualScrap-25069-24169/VirtualScrap-25069-24169/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,8 +80,9 @@ namespace VirtualScrap_25069_24169.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
+            [Required (ErrorMessage ="O campo do {0} é de preenchimento obrigatório")]
+            [EmailAddress(ErrorMessage = "Introduza um email válido")]
+            [StringLength(100, ErrorMessage = "Não pode ter mais de 100 carateres")]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
@@ -89,8 +90,8 @@ namespace VirtualScrap_25069_24169.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required (ErrorMessage ="É obigatório preencher a {0}.")]
+            [StringLength(100, ErrorMessage = "A {0} deve ter no minimo 6 carateres, 1 letra maiúscula, 1 carater especial, e 1 digito.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -101,7 +102,7 @@ namespace VirtualScrap_25069_24169.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "As passwords não coincidem.")]
             public string ConfirmPassword { get; set; }
 
             public MyUser MyUser { get; set; }
