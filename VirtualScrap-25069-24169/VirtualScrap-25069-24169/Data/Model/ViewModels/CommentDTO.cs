969d8531ea41a181eb10cc7a07cce90bb24944e7
Andral-6821 <aluno25069@ipt.pt>
2026-07-07 18:01:29 +0100

    Data Transfer objects concluído.

---
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/ViewModels/CommentDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VirtualScrap_25069_24169.Models.ViewModels
+{
+    public class CommentDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A Descrição é de escrita obrigatória")]
+        [StringLength(500)]
+        public string Description { get; set; } = "";
+
+        public DateTime CommentDate { get; set; }
+
+        // Chaves Estrangeiras exatas do  modelo Comment.cs
+        public int? AutorFK { get; set; }
+
+        [Required(ErrorMessage = "O ID do destinatário (RecipientFK) é obrigatório.")]
+        public int RecipientFK { get; set; }
+
+        [Required(ErrorMessage = "O Rating é de preenchimento obrigatório.")]
+        [Range(1, 5, ErrorMessage = "O Rating deve ser um valor entre 1 e 5.")]
+        public int Rating { get; set; } = 5;
+
+        // Propriedades auxiliares para as leituras (GET) virem limpas no JSON
+        public string AutorName { get; set; } = "";
+        public string RecipientName { get; set; } = "";
+    }
+}
---
+++ VirtualScrap-25069-24169/VirtualScrap-25069-24169/Data/Model/ViewModels/PostCommentDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VirtualScrap_25069_24169.Models.ViewModels
+{
+    public class PostCommentDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A Descrição é de escrita obrigatória")]
+        [StringLength(500)]
+        public string Description { get; set; } = "";
+
+        public DateTime CommentDate { get; set; }
+
+        // Chaves estrangeiras obrigatórias para associar á base de dados 
+        public int? AutorFK { get; set; }
+
+        [Required(ErrorMessage = "O ID do post comentado é obrigatório.")]
+        public int PostFK { get; set; }
+
+        // Propriedades auxiliares para as leituras (GET) serem legíveis em JSON
+        public string AutorName { get; set; } = "";
+        public string CommentedPostTitle { get; set; } = "";
+    }
+}
